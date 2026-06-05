c7ce49290437f64a35efcb7763954e98b4ac2923
salmouiz <sa.lmouiz@edu.umi.ac.ma>
2026-06-05 21:34:04 +0100

    Update ContratDAO.java

--- src/main/java/dao/ContratDAO.java
+++ src/main/java/dao/ContratDAO.java
@@ -8,90 +8,106 @@ import java.util.List;
 
 public class ContratDAO implements CommonDAO<Contrat> {
 
-    private Contrat map(ResultSet rs) throws SQLException {
-        Contrat ct = new Contrat();
-        ct.setId(rs.getInt("id"));
-        ct.setDateDebut(rs.getDate("date_debut"));
-        ct.setDateFin(rs.getDate("date_fin"));
-        ct.setMontantTotal(rs.getDouble("montant_total"));
-        ct.setIdClient(rs.getInt("id_client"));
-        ct.setIdVoiture(rs.getInt("id_voiture"));
-        return ct;
-    }
-
     @Override
     public List<Contrat> selectAll() {
         List<Contrat> liste = new ArrayList<>();
+        Connection c = ConnectDB.getConnection();
         String query = "SELECT * FROM contrat";
-        try (Connection c = ConnectDB.getConnection();
+        try {
              Statement st = c.createStatement();
-             ResultSet rs = st.executeQuery(query)) {
-            while (rs.next()) liste.add(map(rs));
+             ResultSet rs = st.executeQuery(query);
+            while (rs.next()){
+                Contrat ct = new Contrat();
+				ct.setId(rs.getInt("id"));
+				ct.setDateDebut(rs.getDate("date_debut"));
+				ct.setDateFin(rs.getDate("date_fin"));
+				ct.setMontantTotal(rs.getDouble("montant_total"));
+				ct.setIdClient(rs.getInt("id_client"));
+				ct.setIdVoiture(rs.getInt("id_voiture"));
+                liste.add(ct);
+            }
         } catch (SQLException e) { e.printStackTrace(); }
         return liste;
     }
 
     @Override
     public void add(Contrat obj) {
+        Connection c = ConnectDB.getConnection();
         String query = "INSERT INTO contrat (date_debut, date_fin, montant_total, id_client, id_voiture) VALUES (?, ?, ?, ?, ?)";
-        try (Connection c = ConnectDB.getConnection();
-             PreparedStatement ps = c.prepareStatement(query)) {
-            ps.setDate(1, obj.getDateDebut());
-            ps.setDate(2, obj.getDateFin());
-            ps.setDouble(3, obj.getMontantTotal());
-            ps.setInt(4, obj.getIdClient());
-            ps.setInt(5, obj.getIdVoiture());
+        try{
+            PreparedStatement ps = c.prepareStatement(query);
+            ps.setId(1, obj.getId());
+            ps.setDate(2, obj.getDateDebut());
+            ps.setDate(3, obj.getDateFin());
+            ps.setDouble(4, obj.getMontantTotal());
+            ps.setInt(5, obj.getIdClient());
+            ps.setInt(6, obj.getIdVoiture());
             ps.executeUpdate();
         } catch (SQLException e) { e.printStackTrace(); }
     }
 
     @Override
     public void delete(int id) {
+        Connection c = ConnectDB.getConnection();
         String query = "DELETE FROM contrat WHERE id = ?";
-        try (Connection c = ConnectDB.getConnection();
-             PreparedStatement ps = c.prepareStatement(query)) {
+        try {
+            PreparedStatement ps = c.prepareStatement(query);
             ps.setInt(1, id);
-            ps.executeUpdate();
+            int n = ps.executeUpdate();
+            ps.close();
         } catch (SQLException e) { e.printStackTrace(); }
     }
 
     @Override
     public void update(Contrat obj) {
+        Connection c = ConnectDB.getConnection();
         String query = "UPDATE contrat SET date_debut=?, date_fin=?, montant_total=?, id_client=?, id_voiture=? WHERE id=?";
-        try (Connection c = ConnectDB.getConnection();
-             PreparedStatement ps = c.prepareStatement(query)) {
-            ps.setDate(1, obj.getDateDebut());
-            ps.setDate(2, obj.getDateFin());
-            ps.setDouble(3, obj.getMontantTotal());
-            ps.setInt(4, obj.getIdClient());
-            ps.setInt(5, obj.getIdVoiture());
-            ps.setInt(6, obj.getId());
+        try {
+            PreparedStatement ps = c.prepareStatement(query);
+            ps.setInt(1, obj.getId());
+            ps.setDate(2, obj.getDateDebut());
+            ps.setDate(3, obj.getDateFin());
+            ps.setDouble(4, obj.getMontantTotal());
+            ps.setInt(5, obj.getIdClient());
+            ps.setInt(6, obj.getIdVoiture());
             ps.executeUpdate();
         } catch (SQLException e) { e.printStackTrace(); }
     }
 
     @Override
     public Contrat getById(int id) {
+        Connection c = ConnectDB.getConnection();
         String query = "SELECT * FROM contrat WHERE id = ?";
         Contrat ct = null;
-        try (Connection c = ConnectDB.getConnection();
-             PreparedStatement ps = c.prepareStatement(query)) {
+        try {
+            PreparedStatement ps = c.prepareStatement(query);
             ps.setInt(1, id);
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) ct = map(rs);
+            ResultSet rs = ps.executeQuery()
+            if (rs.next()){
+                ct = new Contrat();
+				ct.setId(rs.getInt("id"));
+				ct.setDateDebut(rs.getDate("date_debut"));
+				ct.setDateFin(rs.getDate("date_fin"));
+				ct.setMontantTotal(rs.getDouble("montant_total"));
+				ct.setIdClient(rs.getInt("id_client"));
+				ct.setIdVoiture(rs.getInt("id_voiture"));
             }
+            ps.close();
+            
         } catch (SQLException e) { e.printStackTrace(); }
         return ct;
     }
 
     @Override
     public int getIDMax() {
+        Connection c = ConnectDB.getConnection();
         String query = "SELECT MAX(id) AS max_id FROM contrat";
         int max = 0;
-        try (Connection c = ConnectDB.getConnection();
+        try {
              Statement st = c.createStatement();
-             ResultSet rs = st.executeQuery(query)) {
-            if (rs.next()) max = rs.getInt("max_id") + 1;
+             ResultSet rs = st.executeQuery(query);
+            if (rs.next())
+                max = rs.getInt("max_id") + 1;
         } catch (SQLException e) { e.printStackTrace(); }
         return max;
     }
